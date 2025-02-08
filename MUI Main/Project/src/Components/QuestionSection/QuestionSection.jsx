36a43470de84bde3d901cbfefcb464bb706696f2
KunalGaurav556 <kunalgauravhk556@gmail.com>
2025-02-08 18:36:19 +0530

    Added QUestion Section

--- MUI Main/Project/src/Components/QuestionSection/QuestionSection.jsx
+++ MUI Main/Project/src/Components/QuestionSection/QuestionSection.jsx
@@ -1,22 +1,25 @@
 import { Stack, Typography } from '@mui/material'
 import React from 'react'
+import './QuestionSection.css'
 
 const QuestionSection = () => {
   return (
     <>
-       <Stack direction={'row'}>
+       <Stack direction={'row'} className='mainQuestionStack'>
         <Stack>
-            <Stack>
-                <Typography variant='h2'>Have A Question? Check Out Our FAQ</Typography>
-                <Typography>
+            <Stack className='leftQuestionSide'>
+                <Stack className='headingImageStack'>
+                  <Typography variant='h2' className='questionHeading'>Have A Question? Check Out Our FAQ</Typography>
+                  <Typography>
                     <img src="https://verselion.netlify.app/images/ai/robot.png" alt="" />
-                </Typography>
+                  </Typography>
+                </Stack>
             </Stack>
             <Stack></Stack>
         </Stack>
 
 
-        <Stack></Stack>
+        <Stack className='rightQuestionSide'>ghjhkjhkhgjk</Stack>
        </Stack> 
     </>
   )
